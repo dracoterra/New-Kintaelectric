acfa3cef0e99f4d1dbd30db93080c7477a23c272
Your Name <your.email@example.com>
2025-09-09 00:59:57 -0400

    menu

--- wp-content/themes/kintaelectric/template-parts/header-v4.php
+++ wp-content/themes/kintaelectric/template-parts/header-v4.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Header Template v4 - Top Bar + Header Style
+ * Dynamic version based on header-v1.php, header-v2.php and header-v3.php implementation
  *
  * @package kintaelectric
  */
@@ -10,151 +11,222 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<!-- Top Bar -->
-<div class="top-bar">
-	<div class="container">
-		<div class="row align-items-center">
-			<div class="col-lg-6">
-				<div class="top-bar-left">
-					<ul class="top-bar-links">
-						<li><a href="#"><?php esc_html_e( 'Welcome to Worldwide Electronics Store', 'kintaelectric' ); ?></a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-lg-6">
-				<div class="top-bar-right">
-					<ul class="top-bar-links">
-						<li><a href="#"><i class="ec ec-map-pointer"></i> <?php esc_html_e( 'Store Locator', 'kintaelectric' ); ?></a></li>
-						<li><a href="#"><i class="ec ec-transport"></i> <?php esc_html_e( 'Track Your Order', 'kintaelectric' ); ?></a></li>
-						<li><a href="#"><i class="ec ec-shopping-bag"></i> <?php esc_html_e( 'Shop', 'kintaelectric' ); ?></a></li>
-						<li><a href="#"><i class="ec ec-user"></i> <?php esc_html_e( 'My Account', 'kintaelectric' ); ?></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
+<div class="top-bar top-bar-v1 hidden-lg-down d-none d-xl-block">
+	<div class="container clearfix">
+		<ul id="menu-top-bar-left" class="nav nav-inline float-start electro-animate-dropdown flip">
+			<li id="menu-item-5166"
+				class="menu-item menu-item-type-custom menu-item-object-custom menu-item-5166"><a
+					title="Welcome to Worldwide Electronics Store" href="#">Welcome to Worldwide Electronics
+					Store</a></li>
+		</ul>
+		<ul id="menu-top-bar-right" class="nav nav-inline float-end electro-animate-dropdown flip">
+			<li id="menu-item-5167"
+				class="menu-item menu-item-type-custom menu-item-object-custom menu-item-5167"><a
+					title="Store Locator" href="#"><i class="ec ec-map-pointer"></i>Store Locator</a></li>
+			<li id="menu-item-5299"
+				class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5299"><a
+					title="Track Your Order" href="../track-your-order/index.htm"><i
+						class="ec ec-transport"></i>Track Your Order</a></li>
+			<li id="menu-item-5293"
+				class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5293"><a
+					title="Shop" href="../shop/index.htm"><i class="ec ec-shopping-bag"></i>Shop</a></li>
+			<li id="menu-item-5294"
+				class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5294"><a
+					title="My Account" href="../my-account/index.htm"><i class="ec ec-user"></i>My
+					Account</a></li>
+		</ul>
 	</div>
-</div>
+</div><!-- /.top-bar -->
 
-<header id="masthead" class="header-v4 stick-this site-header">
-	<div class="container">
-		<div class="header-content">
-			<div class="row align-items-center">
-				<div class="col-lg-3">
-					<div class="header-logo">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="header-logo-link">
-							<?php
-							if ( has_custom_logo() ) {
-								the_custom_logo();
-							} else {
-								?>
-								<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/logo.svg' ); ?>" alt="<?php bloginfo( 'name' ); ?>" class="header-logo-img">
-								<?php
-							}
-							?>
+
+
+<header id="masthead" class="site-header stick-this header-v6">
+	<div class="container hidden-lg-down d-none d-xl-block">
+		<div class="masthead row align-items-center">
+			<div class="header-logo-area d-flex justify-content-between align-items-center">
+				<div class="header-site-branding">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="header-logo-link">
+						<?php echo kintaelectric_get_logo( 'light' ); ?>
+					</a>
+				</div>
+				<div class="departments-menu-v2">
+					<div class="dropdown">
+						<a href="#" class="departments-menu-v2-title" data-bs-toggle="dropdown">
+							<span><?php esc_html_e( 'Categories', 'kintaelectric' ); ?><i
+									class="departments-menu-v2-icon ec ec-arrow-down-search"></i></span>
 						</a>
+						<ul id="menu-all-departments-menu" class="dropdown-menu yamm">
+							<?php
+							// All Departments Menu - using WordPress menu system
+							wp_nav_menu( array(
+								'theme_location' => 'all-departments',
+								'menu_class'     => 'nav nav-inline yamm',
+								'container'      => false,
+								'fallback_cb'    => 'kintaelectric_all_departments_fallback',
+								'depth'          => 3,
+								'walker'         => new KintaElectric_YAMM_Walker(),
+							) );
+							?>
+																	</ul>
+																</div>
+															</div>
+														</div>
+
+			<form class="navbar-search col" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>"
+				autocomplete="off">
+				<label class="sr-only screen-reader-text visually-hidden" for="search"><?php esc_html_e( 'Search for:', 'kintaelectric' ); ?></label>
+				<div class="input-group">
+					<div class="input-search-field">
+						<input type="text" id="search"
+							class="form-control search-field product-search-field" dir="ltr" value=""
+							name="s" placeholder="<?php esc_attr_e( 'Search for Products', 'kintaelectric' ); ?>" autocomplete="off">
+					</div>
+					<div class="input-group-addon search-categories d-flex">
+						<select name='product_cat' id='electro_header_search_categories_dropdown'
+							class='postform resizeselect'>
+							<option value='0' selected='selected'><?php esc_html_e( 'All Categories', 'kintaelectric' ); ?></option>
+							<?php echo kintaelectric_get_product_categories(); ?>
+						</select>
+					</div>
+					<div class="input-group-btn">
+						<input type="hidden" id="search-param" name="post_type" value="product">
+						<button type="submit" class="btn btn-secondary"><i
+								class="ec ec-search"></i></button>
 					</div>
 				</div>
-				
-				<div class="col-lg-6">
-					<div class="header-search">
-						<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-							<div class="input-group">
-								<select class="form-control cate-dropdown" name="product_cat">
-									<option value=""><?php esc_html_e( 'All Categories', 'kintaelectric' ); ?></option>
-									<?php
-									if ( class_exists( 'WooCommerce' ) ) {
-										$categories = get_terms( array(
-											'taxonomy' => 'product_cat',
-											'hide_empty' => true,
-										) );
-										foreach ( $categories as $category ) {
-											echo '<option value="' . esc_attr( $category->slug ) . '">' . esc_html( $category->name ) . '</option>';
-										}
-									}
-									?>
-								</select>
-								<input type="search" class="form-control" placeholder="<?php echo esc_attr__( 'Search for products...', 'kintaelectric' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-								<button type="submit" class="btn btn-primary">
-									<i class="ec ec-search"></i>
-								</button>
-								<input type="hidden" name="post_type" value="product" />
-							</div>
-						</form>
-					</div>
+			</form>
+			<div class="header-icons col-auto d-flex justify-content-end align-items-center">
+				<?php if ( class_exists( 'YITH_Woocompare' ) ) : ?>
+				<div style="position: relative;" class="header-icon" data-bs-toggle="tooltip"
+					data-bs-placement="bottom" data-bs-title="<?php esc_attr_e( 'Compare', 'kintaelectric' ); ?>">
+					<a href="<?php echo esc_url( add_query_arg( 'action', 'yith-woocompare-view-table', home_url( '/' ) ) ); ?>"
+						class="yith-woocompare-open">
+						<i class="ec ec-compare"></i>
+						<span id="navbar-compare-count"
+							class="navbar-compare-count count header-icon-counter">0</span>
+					</a>
 				</div>
+				<?php endif; ?>
 				
-				<div class="col-lg-3">
-					<div class="header-actions d-flex justify-content-end align-items-center">
-						<?php if ( class_exists( 'WooCommerce' ) ) : ?>
-							<!-- Compare -->
-							<div class="header-action">
-								<a href="#" class="action-link" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="<?php esc_attr_e( 'Compare', 'kintaelectric' ); ?>">
-									<i class="ec ec-compare"></i>
-									<span class="action-count">0</span>
-								</a>
-							</div>
-							
-							<!-- Wishlist -->
-							<div class="header-action">
-								<a href="#" class="action-link" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="<?php esc_attr_e( 'Wishlist', 'kintaelectric' ); ?>">
-									<i class="ec ec-favorites"></i>
-									<span class="action-count">0</span>
-								</a>
-							</div>
-							
-							<!-- User Account -->
-							<div class="header-action dropdown">
-								<a href="#" class="action-link dropdown-toggle" data-bs-toggle="dropdown" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="<?php esc_attr_e( 'My Account', 'kintaelectric' ); ?>">
-									<i class="ec ec-user"></i>
-								</a>
-								<div class="dropdown-menu dropdown-menu-right">
-									<?php if ( is_user_logged_in() ) : ?>
-										<a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>" class="dropdown-item"><?php esc_html_e( 'My Account', 'kintaelectric' ); ?></a>
-										<a href="<?php echo esc_url( wp_logout_url( home_url() ) ); ?>" class="dropdown-item"><?php esc_html_e( 'Logout', 'kintaelectric' ); ?></a>
-									<?php else : ?>
-										<a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>" class="dropdown-item"><?php esc_html_e( 'Login', 'kintaelectric' ); ?></a>
-										<a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>" class="dropdown-item"><?php esc_html_e( 'Register', 'kintaelectric' ); ?></a>
-									<?php endif; ?>
-								</div>
-							</div>
-							
-							<!-- Shopping Cart -->
-							<div class="header-action dropdown">
-								<a href="<?php echo esc_url( wc_get_cart_url() ); ?>" class="action-link dropdown-toggle" data-bs-toggle="dropdown" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="<?php esc_attr_e( 'Cart', 'kintaelectric' ); ?>">
-									<i class="ec ec-shopping-bag"></i>
-									<span class="action-count"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
-								</a>
-								<div class="dropdown-menu dropdown-menu-right">
-									<?php woocommerce_mini_cart(); ?>
-								</div>
-							</div>
+				<?php if ( class_exists( 'YITH_WCWL' ) ) : ?>
+				<div class="header-icon" data-bs-toggle="tooltip" data-bs-placement="bottom"
+					data-bs-title="<?php esc_attr_e( 'Wishlist', 'kintaelectric' ); ?>">
+					<a href="<?php echo esc_url( YITH_WCWL()->get_wishlist_url() ); ?>">
+						<i class="ec ec-favorites"></i>
+						<?php if ( function_exists( 'yith_wcwl_count_all_products' ) ) : ?>
+						<span class="wishlist-count count header-icon-counter"><?php echo yith_wcwl_count_all_products(); ?></span>
 						<?php endif; ?>
-					</div>
+					</a>
 				</div>
+				<?php endif; ?>
+				
+				<div class="header-icon header-icon__user-account dropdown animate-dropdown"
+					data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="<?php esc_attr_e( 'My Account', 'kintaelectric' ); ?>">
+					<a class="dropdown-toggle" href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" data-bs-toggle="dropdown"><i
+							class="ec ec-user"></i></a>
+					<ul class="dropdown-menu dropdown-menu-user-account">
+						<li>
+							<div class="register-sign-in-dropdown-inner">
+								<div class="sign-in">
+									<p><?php esc_html_e( 'Returning Customer ?', 'kintaelectric' ); ?></p>
+									<div class="sign-in-action"><a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>"
+											class="sign-in-button"><?php esc_html_e( 'Sign in', 'kintaelectric' ); ?></a></div>
+								</div>
+								<div class="register">
+									<p><?php esc_html_e( 'Don\'t have an account ?', 'kintaelectric' ); ?></p>
+									<div class="register-action"><a
+											href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>"><?php esc_html_e( 'Register', 'kintaelectric' ); ?></a></div>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+				
+				<div class="header-icon header-icon__cart animate-dropdown dropdown"
+					data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="<?php esc_attr_e( 'Cart', 'kintaelectric' ); ?>">
+					<a class="dropdown-toggle" href="<?php echo esc_url( wc_get_cart_url() ); ?>" data-bs-toggle="dropdown">
+						<i class="ec ec-shopping-bag"></i>
+						<span class="cart-items-count count header-icon-counter"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+						<span class="cart-items-total-price total-price"><?php echo WC()->cart->get_cart_total(); ?></span>
+					</a>
+					<ul class="dropdown-menu dropdown-menu-mini-cart border-bottom-0-last-child">
+						<li>
+							<div class="widget_shopping_cart_content border-bottom-0-last-child">
+								<?php woocommerce_mini_cart(); ?>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div><!-- /.header-icons -->
+		</div>
+
+	</div>
+
+	<div class="handheld-header-wrap container hidden-xl-up d-xl-none">
+		<div class="handheld-header-v2 row align-items-center handheld-stick-this ">
+			<div class="off-canvas-navigation-wrapper ">
+				<div class="off-canvas-navbar-toggle-buttons clearfix">
+					<button class="navbar-toggler navbar-toggle-hamburger " type="button">
+						<i class="ec ec-menu"></i>
+					</button>
+					<button class="navbar-toggler navbar-toggle-close " type="button">
+						<i class="ec ec-close-remove"></i>
+					</button>
+				</div>
+
+				<div class="off-canvas-navigation light" id="default-oc-header">
+					<ul id="menu-all-departments-menu-1" class="nav nav-inline yamm">
+						<?php
+						// All Departments Menu - using WordPress menu system
+						wp_nav_menu( array(
+							'theme_location' => 'all-departments',
+							'menu_class'     => 'nav nav-inline yamm',
+							'container'      => false,
+							'fallback_cb'    => 'kintaelectric_all_departments_fallback',
+							'depth'          => 3,
+							'walker'         => new KintaElectric_YAMM_Walker(),
+						) );
+						?>
+					</ul>
+				</div>
+			</div>
+			<div class="header-logo">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="header-logo-link">
+					<?php echo kintaelectric_get_logo( 'mobile' ); ?>
+				</a>
+			</div>
+			<div class="handheld-header-links">
+				<ul class="columns-3">
+					<li class="search">
+						<a href=""><?php esc_html_e( 'Search', 'kintaelectric' ); ?></a>
+						<div class="site-search">
+							<div class="widget woocommerce widget_product_search">
+								<form role="search" method="get" class="woocommerce-product-search"
+									action="<?php echo esc_url( home_url( '/' ) ); ?>">
+									<label class="screen-reader-text"
+										for="woocommerce-product-search-field-0"><?php esc_html_e( 'Search for:', 'kintaelectric' ); ?></label>
+									<input type="search" id="woocommerce-product-search-field-0"
+										class="search-field" placeholder="<?php esc_attr_e( 'Search products…', 'kintaelectric' ); ?>" value=""
+										name="s">
+									<button type="submit" value="<?php esc_attr_e( 'Search', 'kintaelectric' ); ?>" class=""><?php esc_html_e( 'Search', 'kintaelectric' ); ?></button>
+									<input type="hidden" name="post_type" value="product">
+								</form>
+							</div>
+						</div>
+					</li>
+					<li class="my-account">
+						<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>"><i class="ec ec-user"></i></a>
+					</li>
+					<li class="cart">
+						<a class="footer-cart-contents" href="<?php echo esc_url( wc_get_cart_url() ); ?>"
+							title="<?php esc_attr_e( 'View your shopping cart', 'kintaelectric' ); ?>">
+							<i class="ec ec-shopping-bag"></i>
+							<span class="cart-items-count count"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+						</a>
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
-</header>
 
-<!-- Main Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<div class="container">
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-					'container'      => false,
-					'menu_class'     => 'navbar-nav me-auto',
-					'fallback_cb'    => 'wp_page_menu',
-				)
-			);
-			?>
-		</div>
-	</div>
-</nav>
-
+</header><!-- #masthead -->
