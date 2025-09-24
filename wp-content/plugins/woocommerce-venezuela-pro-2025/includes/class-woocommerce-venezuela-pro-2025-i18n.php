9bfa9aaa4ac1c723497f528e257763cad52697db
Your Name <your.email@example.com>
2025-09-24 16:40:24 -0400

    Add initial implementation of WooCommerce Venezuela Pro 2025 plugin
    - Introduced core plugin files including activation, deactivation, and internationalization functionalities.
    - Defined admin and public classes for managing plugin-specific actions and hooks.
    - Included CSS and JS files for both admin and public-facing components.
    - Established a structured directory layout for better organization and maintainability.
    - Added licensing and readme files to provide essential information about the plugin.

---
+++ wp-content/plugins/woocommerce-venezuela-pro-2025/includes/class-woocommerce-venezuela-pro-2025-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://https://artifexcodes.com/
+ * @since      1.0.0
+ *
+ * @package    Woocommerce_Venezuela_Pro_2025
+ * @subpackage Woocommerce_Venezuela_Pro_2025/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Woocommerce_Venezuela_Pro_2025
+ * @subpackage Woocommerce_Venezuela_Pro_2025/includes
+ * @author     ronald alvarez <ronaldalv2025@gmail.com>
+ */
+class Woocommerce_Venezuela_Pro_2025_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'woocommerce-venezuela-pro-2025',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
